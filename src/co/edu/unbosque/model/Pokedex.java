9e435806555fba966c7577d208ccde8e07143cf9
Brayan Steven Useche Palomino <buseche@unbosque.edu.co>
2023-03-10 15:01:08 -0500

    delete-search-filter

--- src/co/edu/unbosque/model/Pokedex.java
+++ src/co/edu/unbosque/model/Pokedex.java
@@ -12,4 +12,33 @@ public class Pokedex {
         System.out.println("Pokemon registrado exitosamente.");
     }
 
+    public Pokemon buscarPokemonPorNombre(String nombre) {
+        for (Pokemon pokemon : this.pokemones) {
+            if (pokemon.getNombre().equalsIgnoreCase(nombre)) {
+                return pokemon;
+            }
+        }
+        return null;
+    }
+
+    public List<Pokemon> filtrarPorTipo(String tipo) {
+        List<Pokemon> resultado = new ArrayList<>();
+        for (Pokemon pokemon : this.pokemones) {
+            if (pokemon.getTipo().equalsIgnoreCase(tipo)) {
+                resultado.add(pokemon);
+            }
+        }
+        return resultado;
+    }
+
+    public boolean eliminarPokemon(String nombre) {
+        for (Pokemon pokemon : pokemonList) {
+            if (pokemon.getNombre().equalsIgnoreCase(nombre)) {
+                pokemonList.remove(pokemon);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
